7917d964660ce5b2aef264bd34c201a342dc156b
Dattatray Salunkhe <dattatraysalunkhe832@gmail.com>
2024-09-05 21:35:03 +0530

    fix:routing

--- api/index.js
+++ api/index.js
@@ -1,10 +1,29 @@
 import cookieParser from 'cookie-parser'
 import express from 'express'
+import {DB_Name} from "./Utils/constant.js"
+import mongoose from 'mongoose'
+import { configDotenv } from 'dotenv'
+
+configDotenv() // this for env access all over
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
-app.listen(port || 4000, () => {
+const dbConnect = async() => {
+
+    try {
+        const connected = await mongoose.connect(`${process.env.MONGODBURL}/${DB_Name}`)
+    
+        console.log("Mongodb Connected")
+    } catch (error) {
+        console.log("Mongodb connection failed"),error
+    }
+
+}
+
+dbConnect()
+
+app.listen(port , () => {
     console.log(`port is listing ${port}`)
 })
 
@@ -14,3 +33,4 @@ app.use(cookieParser())
 app.use("/api", (req,res) =>{
    res.send("from feat1")
 })
+
